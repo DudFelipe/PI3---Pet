261d102d6408f247e85ee43f7caba27457df13bd
Eduardo Felipe Donadio de Almeida Serra <efelipe10@hotmail.com>
2018-11-08 22:28:26 -0200

    - Inserção de pets funcionando; - Falta terminar a parte de alterar; - Fazer a parte de exclusão;

---
+++ Petshop/src/main/java/br/senac/tads/pi3a/petshop/Servlets/AlterarPetServlet.java
@@ -0,0 +1,93 @@
+package br.senac.tads.pi3a.petshop.Servlets;
+
+import br.senac.tads.pi3a.petshop.BLL.ClienteBLL;
+import br.senac.tads.pi3a.petshop.BLL.PetBLL;
+import br.senac.tads.pi3a.petshop.Modelos.Cliente;
+import br.senac.tads.pi3a.petshop.Modelos.Pet;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "AlterarPetServlet", urlPatterns = {"/AlterarPetServlet"})
+public class AlterarPetServlet extends HttpServlet {
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        int id = Integer.parseInt(request.getParameter("id"));
+        
+        Pet p = new Pet();
+        List<Pet> listaPets = null;
+        List<Cliente> listaClientes = null;
+        
+        try{
+            p = PetBLL.obterPet(id);
+            
+            listaPets = PetBLL.listar();
+            listaClientes = ClienteBLL.listar();
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        
+        request.setAttribute("pet", p);
+        request.setAttribute("pets", listaPets);
+        request.setAttribute("clientes", listaClientes);
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/pets.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        int id = Integer.parseInt(request.getParameter("id"));
+        
+        Pet p = new Pet();
+        List<Pet> listaPets = null;
+        List<Cliente> listaClientes = null;
+        
+        try{
+            p = PetBLL.obterPet(id);
+            
+            
+            Cliente c = new Cliente();
+            c = ClienteBLL.obterCliente(Integer.parseInt(request.getParameter("dono")));
+            p.setIdCliente(c);
+            
+            p.setNome(request.getParameter("nomepet"));
+            
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            p.setNascimento(sdf.parse(request.getParameter("nascimento")));
+            
+            p.setEspecie(request.getParameter("especie"));
+            p.setPorte("tamanhoanimal");
+            
+            PetBLL.alterar(p);
+            
+            listaPets = PetBLL.listar();
+            listaClientes = ClienteBLL.listar();
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+     
+        request.setAttribute("pet", p);
+        request.setAttribute("pets", listaPets);
+        request.setAttribute("clientes", listaClientes);
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/pets.jsp");
+        dispatcher.forward(request, response);
+        
+    }
+}
---
+++ Petshop/src/main/java/br/senac/tads/pi3a/petshop/Servlets/PetsServlet.java
@@ -0,0 +1,81 @@
+package br.senac.tads.pi3a.petshop.Servlets;
+
+import br.senac.tads.pi3a.petshop.BLL.ClienteBLL;
+import br.senac.tads.pi3a.petshop.BLL.PetBLL;
+import br.senac.tads.pi3a.petshop.DAO.ClienteDAO;
+import br.senac.tads.pi3a.petshop.DAO.PetDAO;
+import br.senac.tads.pi3a.petshop.Modelos.Cliente;
+import br.senac.tads.pi3a.petshop.Modelos.Pet;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "PetsServlet", urlPatterns = {"/PetsServlet"})
+public class PetsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        List<Cliente> clientes = null;
+        List<Pet> pets = null;
+        
+        try {
+            clientes = ClienteBLL.listar();
+            pets = PetBLL.listar();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        request.setAttribute("clientes", clientes);
+        request.setAttribute("pets", pets);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/pets.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Pet p = new Pet();
+        List<Pet> listaPets = null;
+        List<Cliente> listaClientes = null;
+
+        Cliente c = new Cliente();
+
+        try {
+            c = ClienteBLL.obterCliente(Integer.parseInt(request.getParameter("dono")));
+            
+            p.setIdCliente(c);
+            p.setNome(request.getParameter("nomepet"));
+            
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            p.setNascimento(sdf.parse(request.getParameter("nascimento")));
+            
+            p.setEspecie(request.getParameter("especie"));
+            p.setPorte("tamanhoanimal");
+            
+            listaPets = PetBLL.listar();
+            listaClientes = ClienteBLL.listar();
+            
+            PetBLL.inserir(p);
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        request.setAttribute("pets", listaPets);
+        request.setAttribute("clientes", listaClientes);
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/pets.jsp");
+        dispatcher.forward(request, response);
+    }
+}
