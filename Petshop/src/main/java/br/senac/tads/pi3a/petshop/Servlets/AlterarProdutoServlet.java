25f1d19bf55466d7d444a67108c2ab80a5543df2
Giovane Barreira <giovane_barreira@hotmail.com>
2018-11-27 19:29:27 -0200

    .

--- Petshop/src/main/java/br/senac/tads/pi3a/petshop/Servlets/AlterarProdutoServlet.java
+++ Petshop/src/main/java/br/senac/tads/pi3a/petshop/Servlets/AlterarProdutoServlet.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+//<<<<<<< HEAD
 ///*
 // * To change this license header, choose License Headers in Project Properties.
 // * To change this template file, choose Tools | Templates
@@ -71,51 +71,51 @@
 //    }// </editor-fold>
 //
 //}
-=======
-package br.senac.tads.pi3a.petshop.Servlets;
->>>>>>> Diogo
-
-import br.senac.tads.pi3a.petshop.BLL.ProdutoBLL;
-import br.senac.tads.pi3a.petshop.Modelos.Produto;
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-public class AlterarProdutoServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-       int id = Integer.parseInt(request.getParameter("id"));
-       
-       Produto p = null;
-       List<Produto> produtos = null;
-       
-       try{
-           p = ProdutoBLL.ObterProduto(id);
-           produtos = ProdutoBLL.listar();
-       }
-    }
-
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    } </editor-fold>
-
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> Diogo
+//=======
+//package br.senac.tads.pi3a.petshop.Servlets;
+//>>>>>>> Diogo
+//
+//import br.senac.tads.pi3a.petshop.BLL.ProdutoBLL;
+//import br.senac.tads.pi3a.petshop.Modelos.Produto;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//
+//public class AlterarProdutoServlet extends HttpServlet {
+//
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        
+//       int id = Integer.parseInt(request.getParameter("id"));
+//       
+//       Produto p = null;
+//       List<Produto> produtos = null;
+//       
+//       try{
+//           p = ProdutoBLL.ObterProduto(id);
+//           produtos = ProdutoBLL.listar();
+//       }
+//    }
+//
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    @Override
+//    public String getServletInfo() {
+//        return "Short description";
+//    } </editor-fold>
+//
+//<<<<<<< HEAD
+//}
+//=======
+//}
+//>>>>>>> Diogo
